b01bc2bcb2acca63987febb642bb258847ced565
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-11-28 20:10:44 +0530

    Create PageTransition.jsx

---
+++ components/PageTransition.jsx
@@ -0,0 +1,11 @@
+import { AnimatePresence } from "framer-motion";
+
+const PageTransition = ({children}) => {
+    return (
+        <AnimatePresence>
+            {children}
+        </AnimatePresence>
+    )
+}
+
+export default PageTransition;
