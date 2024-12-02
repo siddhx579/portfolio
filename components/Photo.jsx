e849091f0d4b11345298303506e80102a7a9884f
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-12-02 15:05:35 +0530

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,6 +10,10 @@ const Photo = () => {
                 <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease:"easeInOut"},}} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
                     <Image src="/assets/photo.png" priority quality={100} fill alt="" className="object-contain" />
                 </motion.div>
+
+                <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" xmlns="http://www.w3.org/2000/svg">
+                    <motion.circle cx="253" cy="253" r="250" stroke="#00ff99" />
+                </motion.svg>
             </motion.div>
         </div>
     )
