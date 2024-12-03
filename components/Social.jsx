b3a2fe790fa4d328d0d88863205778ece6edf1c8
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-12-03 22:11:35 +0530

    adding links to socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 import {FaGithub, FaLinkedinIn, FaInstagram} from 'react-icons/fa'
 
 const socials = [
-    {icon: <FaGithub />, path: ''},
-    {icon: <FaLinkedinIn />, path: ''},
-    {icon: <FaInstagram />, path: ''},
+    {icon: <FaGithub />, path: 'https://github.com/siddhx579'},
+    {icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/siddhant579/'},
+    {icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_211/'},
 ];
 
 const Social = ({containerStyles, iconStyles}) => {
     return (
         <div className={containerStyles}>
             {socials.map((item, index) => {
-                return <Link key={index} href={item.path} className={iconStyles}>{item.icon}</Link>
+                return <Link key={index} href={item.path} className={iconStyles} target="_blank" rel="noopener noreferrer">{item.icon}</Link>
             })}
         </div>
     );
