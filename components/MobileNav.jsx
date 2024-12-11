1d40451709200ac36824d8515983790f6000c103
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-12-11 23:23:07 +0530

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,7 +36,7 @@ const MobileNav = () => {
                 <CiMenuFries className='text-[32px] text-accent' />
             </SheetTrigger>
             <SheetContent className="flex flex-col">
-                <div className="mt-32 mb-40 text-center text-2xl">
+                <div className="mt-20 mb-20 text-center text-2xl">
                     <Link href="/">
                         <h1 className="text-4xl font-semibold">
                             Siddhant<span className='text-accent'>.</span>
