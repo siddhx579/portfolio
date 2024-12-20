543ef99b1eea0ce0b1dd92f0a8c420660dea174e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-12-20 23:54:45 +0530

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from 'react-icons/bs';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+    {
+        num: "01",
+        category: "frontend",
+        title: "Project 1",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius voluptatum numquam qui temporibus, consequuntur minus?"
+    },
+    {
+        num: "02",
+        category: "mern stack",
+        title: "Project 2",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda magnam eos numquam!",
+    },
+    {
+        num: "03",
+        category: "backend",
+        title: "Project 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, nemo beatae. Natus voluptatibus ipsa ut?",
+    },
+];
+
 const Work = () => {
     return (
         <div>work page</div>
