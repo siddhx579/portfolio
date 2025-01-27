cb94750fa3d4c12216e6311495b040ea45d95429
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-27 20:41:15 +0530

    deploy

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Web Developer</span>
             <h1 className="h1">
-              Hello I'm <br /> <span className="text-accent">Siddhant Kapoor</span>
+              Hello I&apos;m <br /> <span className="text-accent">Siddhant Kapoor</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at creating elegant digital experiences and I am proficient in various programming languages and technologies.
