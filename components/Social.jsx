ef2d258d97e9f45cac0e094e0f4fdb488378160b
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-02-14 17:32:31 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -4,7 +4,7 @@ import {FaGithub, FaLinkedinIn, FaInstagram} from 'react-icons/fa'
 const socials = [
     {icon: <FaGithub />, path: 'https://github.com/siddhx579'},
     {icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/siddhant579/'},
-    {icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_211/'},
+    {icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_21/'},
 ];
 
 const Social = ({containerStyles, iconStyles}) => {
