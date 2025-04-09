4ea91a4fb5cd622d90761f630743eac9c65671d9
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-10 00:34:27 +0530

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet';
+import {Sheet, SheetContent, SheetTitle, SheetTrigger} from '@/components/ui/sheet';
 import {usePathname} from 'next/navigation';
 import Link from 'next/link';
 import {CiMenuFries} from 'react-icons/ci';
@@ -36,6 +36,7 @@ const MobileNav = () => {
                 <CiMenuFries className='text-[32px] text-accent' />
             </SheetTrigger>
             <SheetContent className="flex flex-col">
+                <SheetTitle className='sr-only'>Hidden</SheetTitle>
                 <div className="mt-20 mb-20 text-center text-2xl">
                     <Link href="/">
                         <h1 className="text-4xl font-semibold">
