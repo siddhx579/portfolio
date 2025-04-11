f71c62f14c4d955c253607b071dd6089b827d53a
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-11 20:13:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		container: {
@@ -22,18 +22,24 @@ module.exports = {
 		},
 		extend: {
 			keyframes: {
+				gradient: {
+					'0%': { backgroundPosition: '0% 50%' },
+					'50%': { backgroundPosition: '100% 50%' },
+					'100%': { backgroundPosition: '0% 50%' },
+				},
 				"accordion-down": {
-					from: {height: "0"},
-					to: {height: "var(--radix-accordion-content-height)"},
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
 				},
 				"accordion-up": {
-					from: {height: "var(--radix-accordion-content-height)"},
-					to: {height: "0"},
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
 				},
 			},
-			animation:{
+			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+				gradient: 'gradient 8s linear infinite',
 			},
 			colors: {
 				primary: "#1c1c22",
