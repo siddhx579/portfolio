d7ceb04fe9175ecd2eb6b0775250bb479ce3dbd1
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-12 10:28:29 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <span className="text-xl">Web Developer</span>
             <h1 className="h1">
               Hello I&apos;m <br />
-              <span className="text-accent">Siddhant Kapoor</span>
+              <span className="animated-gradient">Siddhant Kapoor</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I excel at creating elegant digital experiences and I am proficient in various programming languages and technologies.
