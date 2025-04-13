9c07b5b90d0bb70bac7ed28e9540dfb59fb0f495
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-13 11:42:24 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
                     <div className="xl:w-[54%] order-2 xl:order-none">
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
                             <h3 className="text-4xl text-accent ">Let&apos;s work together</h3>
-                            <p className="text-white/60">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In fugiat minima molestiae deserunt exercitationem!</p>
+                            <p className="text-white/60">Got a project or idea you'd like to discuss? Fill out the form and let's bring it to life together!</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="firstname" placeholder="Firstname" />
                                 <Input type="lastname" placeholder="Lastname" />
@@ -47,9 +47,9 @@ const Contact = () => {
                                 <SelectContent>
                                     <SelectGroup>
                                         <SelectLabel>Select a service</SelectLabel>
-                                        <SelectItem value="est">Web Development</SelectItem>
-                                        <SelectItem value="cst">UI/UX Developer</SelectItem>
-                                        <SelectItem value="mst">Logo Design</SelectItem>
+                                        <SelectItem value="est">Frontend Development</SelectItem>
+                                        <SelectItem value="cst">Full Stack MERN Developer</SelectItem>
+                                        <SelectItem value="mst">Project Collab</SelectItem>
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
