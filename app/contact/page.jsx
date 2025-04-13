24aaafce7d49e2d28573b464c0c1a912678a9a64
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-13 11:44:53 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
                     <div className="xl:w-[54%] order-2 xl:order-none">
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
                             <h3 className="text-4xl text-accent ">Let&apos;s work together</h3>
-                            <p className="text-white/60">Got a project or idea you'd like to discuss? Fill out the form and let's bring it to life together!</p>
+                            <p className="text-white/60">Got a project or idea you&apos;d like to discuss? Fill out the form and let&apos;s bring it to life together!</p>
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="firstname" placeholder="Firstname" />
                                 <Input type="lastname" placeholder="Lastname" />
