639c76ee03321414843933ff8cffc5396b2d523e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-13 12:04:58 +0530

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -11,16 +11,16 @@ const links = [
         path: '/',
     },
     {
-        name: 'services',
-        path: '/services',
-    },
-    {
         name: 'resume',
         path: '/resume',
     },
     {
         name: 'work',
         path: '/work',
+    },
+    {
+        name: 'blogs',
+        path: '/blogs',
     },
     {
         name: 'contact',
