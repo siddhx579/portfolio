98b7184373468eed7c189a3b8b2e2ed69b8e3d44
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-16 22:47:04 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn, FaInstagram} from 'react-icons/fa'
+import { FaGithub, FaLinkedinIn, FaInstagram } from 'react-icons/fa'
+import { FaXTwitter } from "react-icons/fa6";
 
 const socials = [
-    {icon: <FaGithub />, path: 'https://github.com/siddhx579'},
-    {icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/siddhant579/'},
-    {icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_21/'},
+    { icon: <FaGithub />, path: 'https://github.com/siddhx579' },
+    { icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/siddhant579/' },
+    { icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_21/' },
+    { icon: <FaXTwitter />, path: 'https://x.com/siddhx_21' },
 ];
 
-const Social = ({containerStyles, iconStyles}) => {
+const Social = ({ containerStyles, iconStyles }) => {
     return (
         <div className={containerStyles}>
             {socials.map((item, index) => {
