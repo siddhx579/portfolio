cfce5e2d5d76f93cd58d30f769b44d2a618a5d5d
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-16 22:49:39 +0530

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -106,7 +106,7 @@ const Work = () => {
     }
 
     return (
-        <motion.section initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+        <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
