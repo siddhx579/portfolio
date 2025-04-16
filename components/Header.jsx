a0564183127e6d03068118223732487e6c1b0140
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-16 22:49:41 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,27 +5,27 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
-    <header className="py-8 xl:py-12 text-white">
-        <div className="container mx-auto flex justify-between items-center">
-            {}
-            <Link href="/">
-                <h1 className="text-4xl font-semibold">Siddhant <span className="text-accent">.</span></h1>
-            </Link>
-
-            {}
-            <div className="hidden xl:flex items-center gap-8">
-                <Nav />
-                <Link href="/contact">
-                    <Button>Hire me</Button>
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                { }
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">Siddhant <span className="text-accent">.</span></h1>
                 </Link>
-            </div>
 
-            {}
-            <div className="xl:hidden">
-                <MobileNav />
+                { }
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                { }
+                <div className="xl:hidden">
+                    <MobileNav />
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     );
 };
 
