c059224de0fdbb8cf0b325e666bb0a3beae21321
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-17 21:33:21 +0530

    Update Stairs.jsx

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 const stairAnimation = {
     initial: {
-        top: "0%", 
+        top: "0%",
     },
     animate: {
         top: "100%",
@@ -21,9 +21,9 @@ const Stairs = () => {
     return (
         <>
             {[...Array(6)].map((_, index) => {
-                return(
+                return (
                     <motion.div key={index} variants={stairAnimation} initial="initial" animate="animate" exit="exit" transition={{
-                    duration: 0.4, ease: 'easeInOut', delay: reverseIndex(index) * 0.1,
+                        duration: 0.4, ease: 'easeInOut', delay: reverseIndex(index) * 0.1,
                     }} className="h-full w-full bg-white relative" />
                 );
             })}
