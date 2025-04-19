c105c45eb36fe2d4c7c2b26cd2669dd117ac3c78
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-19 23:43:15 +0530

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -49,7 +49,7 @@ const projects = [
             { name: "Node.js" },
             { name: "MongoDB" },
         ],
-        image: "/assets/work/thumb3.png",
+        image: "/assets/work/thumb3.jpg",
         live: "",
         github: "https://github.com/siddhx579/Spotify-Clone",
     },
@@ -63,7 +63,7 @@ const projects = [
             { name: "Prisma" },
             { name: "Tailwind CSS" },
         ],
-        image: "",
+        image: "/assets/work/thumb4.jpg",
         live: "https://modern-tasknext.vercel.app/",
         github: "https://github.com/siddhx579/task-app",
     },
@@ -78,7 +78,7 @@ const projects = [
             { name: "Gemini API" },
             { name: "Prisma" },
         ],
-        image: "",
+        image: "/assets/work/thumb5.jpg",
         live: "https://ai-writeflow.vercel.app/",
         github: "https://github.com/siddhx579/ai-content-generator",
     },
@@ -92,7 +92,7 @@ const projects = [
             { name: "Blynk" },
             { name: "NodeMCU" },
         ],
-        image: "",
+        image: "/assets/work/thumb6.jpg",
         live: "",
         github: "https://github.com/siddhx579/Home-Automation-using-ESP8266",
     },
@@ -106,7 +106,7 @@ const Work = () => {
     }
 
     return (
-        <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+        <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className="min-h-[80vh] flex flex-col justify-center py-4 xl:px-0">
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
@@ -121,41 +121,45 @@ const Work = () => {
                             </ul>
                             <div className="border border-white/20"></div>
                             <div className="flex items-center gap-4">
-                                <Link target="_blank" href={project.live}>
-                                    <TooltipProvider delayDuration={100}>
-                                        <Tooltip>
-                                            <TooltipTrigger className="w-[60px] h-[60px] rounded-full bg-white/5 flex justify-center items-center group">
-                                                <BsArrowUpRight className="text-white text-3xl group-hover:text-accent " />
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>Live project</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                </Link>
-                                <Link target="_blank" href={project.github}>
-                                    <TooltipProvider delayDuration={100}>
-                                        <Tooltip>
-                                            <TooltipTrigger className="w-[60px] h-[60px] rounded-full bg-white/5 flex justify-center items-center group">
-                                                <BsGithub className="text-white text-3xl group-hover:text-accent " />
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>Github repository</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                </Link>
+                                {project.live && (
+                                    <Link target="_blank" href={project.live}>
+                                        <TooltipProvider delayDuration={100}>
+                                            <Tooltip>
+                                                <TooltipTrigger className="w-[60px] h-[60px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                    <BsArrowUpRight className="text-white text-3xl group-hover:text-accent " />
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>Live project</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </Link>
+                                )}
+                                {project.github && (
+                                    <Link target="_blank" href={project.github}>
+                                        <TooltipProvider delayDuration={100}>
+                                            <Tooltip>
+                                                <TooltipTrigger className="w-[60px] h-[60px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                    <BsGithub className="text-white text-3xl group-hover:text-accent " />
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>Github repository</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </Link>
+                                )}
                             </div>
                         </div>
                     </div>
-                    <div className="w-full xl:w-[50%] ">
+                    <div className="w-full xl:w-[50%] rounded-2xl">
                         <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
                             {projects.map((project, index) => {
                                 return <SwiperSlide key={index} className="w-full">
-                                    <div className="h-[410px] relative group flex justify-center items-center bg-pink-50/20">
+                                    <div className="h-[410px] relative group flex justify-center items-center bg-pink-50/20 rounded-2xl">
                                         <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                                        <div className="relative w-full h-full">
-                                            <Image src={project.image} fill className="bject-cover" alt="" />
+                                        <div className="relative w-full h-full rounded-2xl">
+                                            <Image src={project.image} fill className="object-cover rounded-2xl" alt="project image" />
                                         </div>
                                     </div>
                                 </SwiperSlide>;
