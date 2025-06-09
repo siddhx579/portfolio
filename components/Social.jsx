934bb50a771078fb0746c33e3d9a1e814b0808ad
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-09 15:50:11 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -6,7 +6,7 @@ const socials = [
     { icon: <FaGithub />, path: 'https://github.com/siddhx579' },
     { icon: <FaLinkedinIn />, path: 'https://www.linkedin.com/in/siddhant579/' },
     { icon: <FaInstagram />, path: 'https://www.instagram.com/siddhx_21/' },
-    { icon: <FaXTwitter />, path: 'https://x.com/siddhx_21' },
+    { icon: <FaXTwitter />, path: 'https://x.com/siddhx21' },
 ];
 
 const Social = ({ containerStyles, iconStyles }) => {
